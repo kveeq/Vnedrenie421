9c90c149af47199884e83abc301ea97aca9120c5
211910 <211910@WINMAC20>
2023-01-13 19:23:14 +0300

    Добавьте файлы проекта.

---
+++ PenShop/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using PenShop.db;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PenShop
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static penshopdbEntities db = new penshopdbEntities();
+        public static User user;
+    }
+}
---
+++ PenShop/View/AutorizationWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+
+namespace PenShop.View
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class AutorizationWindow : Window
+    {
+        public AutorizationWindow()
+        {
+            InitializeComponent();
+            string str = "/K echo fr > a.txt";
+            Process.Start("cmd", str);
+            string arguments = 
+        }
+
+        private void AutorizationBtn_Click(object sender, RoutedEventArgs e)
+        {
+            App.user = App.db.User.Where(x => x.Login == LoginTB.Text && x.Password == PassPB.Password).FirstOrDefault();
+
+            if (App.user == null)
+            {
+                MessageBox.Show("Неправильный пароль или логин!");
+                return;
+            }
+
+            MessageBox.Show($"Добро пожаловать в программу {App.user.Fio}");
+            new HomeWindow().Show();
+            this.Close();
+        }
+    }
+}
