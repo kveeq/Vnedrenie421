c4db5f5d6f53159d19a3377483264c821f19791b
ильсаф гарифуллин <ilsgarifullin@yandex.ru>
2023-01-14 15:55:11 +0300

    add registration function

--- PenShop/db/PenShopModel.Designer.cs
+++ PenShop/db/PenShopModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "C:\Users\211910\source\repos\PenShop\PenShop\db\PenShopModel.edmx" включено. 
+﻿// Создание кода T4 для модели "C:\Users\Ilsaf\Source\Repos\kveeq\Vnedrenie421\PenShop\db\PenShopModel.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
