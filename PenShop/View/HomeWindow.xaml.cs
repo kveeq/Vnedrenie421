e073c22ad1935360cca2435968dc2b3345b9f9d8
211910 <211910@K1210-06>
2023-01-16 17:33:20 +0300

    add svor menu

--- PenShop/View/HomeWindow.xaml.cs
+++ PenShop/View/HomeWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,9 +20,36 @@ namespace PenShop.View
     /// </summary>
     public partial class HomeWindow : Window
     {
+        bool isOpen = true;
         public HomeWindow()
         {
             InitializeComponent();
+            //HomePnl.Width = HomePnl.ActualWidth;
+        }
+
+        private async void OpenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (isOpen)
+            {
+                await ResizePnl(HomePnl, -1, 10);
+                HomePnl.Width++;
+            }
+            else
+            {
+                await ResizePnl(HomePnl, 1, 10);
+                HomePnl.Width--;
+            }
+
+            isOpen = !isOpen;
+        }
+
+        private async Task ResizePnl(Grid pnl, int resizeValue, int duration)
+        {
+            while (pnl.Width > 50 && pnl.Width < 151)
+            {
+                pnl.Width += resizeValue;
+                await Task.Run(() => Thread.Sleep(duration));
+            }
         }
     }
 }
