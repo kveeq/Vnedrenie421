fb9fb4aac565e74ca5259f102e2bde119d6d289e
ильсаф гарифуллин <ilsgarifullin@yandex.ru>
2023-01-20 17:26:11 +0300

    update

--- PenShop/View/HomeWindow.xaml.cs
+++ PenShop/View/HomeWindow.xaml.cs
@@ -11,7 +11,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MaterialDesignThemes;
 
 namespace PenShop.View
 {
@@ -21,9 +23,12 @@ namespace PenShop.View
     public partial class HomeWindow : Window
     {
         bool isOpen = true;
+        public NavigationService service;
+
         public HomeWindow()
         {
             InitializeComponent();
+            service = frma.NavigationService;
             //HomePnl.Width = HomePnl.ActualWidth;
         }
 
@@ -45,11 +50,39 @@ namespace PenShop.View
 
         private async Task ResizePnl(Grid pnl, int resizeValue, int duration)
         {
+            var vs1 = Visibility.Collapsed;
+            var vs2 = Visibility.Visible; 
             while (pnl.Width > 50 && pnl.Width < 151)
             {
                 pnl.Width += resizeValue;
                 await Task.Run(() => Thread.Sleep(duration));
             }
+
+                
+            if(resizeValue > 0)
+            {
+                vs1 = Visibility.Collapsed;
+                vs2 = Visibility.Visible;
+            }
+            if(resizeValue < 0)
+            {
+                vs1 = Visibility.Visible;
+                vs2 = Visibility.Collapsed;
+            }
+
+            HomeNavigateBtn.Visibility = vs1;
+            HomeNavigateTxt.Visibility = vs2;
+            PensNavigateBtn.Visibility = vs1;
+            PensNavigateTxt.Visibility = vs2;
+            ClientsNavigateBtn.Visibility = vs1;
+            ClientsNavigateTxt.Visibility = vs2;
+        }
+
+        private void PensBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var a = new PensWindow();
+            service.Navigate(a);
+            var items = App.db.Pen.ToList();
         }
     }
 }
---
+++ PenShop/View/PensWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Windows.Controls;
+
+namespace PenShop.View
+{
+    /// <summary>
+    /// Логика взаимодействия для PensWindow.xaml
+    /// </summary>
+    public partial class PensWindow : Page
+    {
+        public PensWindow()
+        {
+            InitializeComponent();
+            PensLstVIew.ItemsSource = App.db.Pen.ToList();
+            var pen = App.db.Pen.Take(2);
+        }
+    }
+}
